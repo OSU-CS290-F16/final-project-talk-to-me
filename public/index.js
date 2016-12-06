9bec506ae694c94f668a35ee12a60d424f8c6e08
revnak <joshuanovak@yahoo.com>
2016-12-05 16:40:45 -0800

    Uploading Javascript

--- public/index.js
+++ public/index.js
@@ -1,19 +1,30 @@
 
+var lastline = "";
+
 function addNewEntry(event) {
+  console.log(event.code);
   if(event.code == "Enter"){
+
     var entryText = document.getElementsByClassName('entry')[0];
     var entryTextValue = entryText.value;
-    entryText.value = "";
-    responseValue = "";
-    for(var i = 0; i < entryTextValue.length; i++){
-      responseValue += entryTextValue[entryTextValue.length-i-1];
-    }
-
-    var log = document.getElementsByClassName('chatlog')[0];
-    log.value += entryTextValue + "\n";
-    log.value += responseValue + "\n";
-    log.scrollTop = log.scrollHeight;
-
+    var postUrl = '/sendingstrings';
+    var postRequest = new XMLHttpRequest();
+    postRequest.open('POST', postUrl);
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.addEventListener('load', function (event) {
+      console.log("inside");
+      responseValue = event.target.response;
+      var log = document.getElementsByClassName('chatlog')[0];
+      lastline = responseValue;
+      log.value += entryTextValue + "\n";
+      log.value += responseValue + "\n";
+      log.scrollTop = log.scrollHeight;
+    });
+    console.log("inside");
+    postRequest.send(JSON.stringify({
+    line: lastline,
+    response: entryTextValue
+  }));
   }
 }
 
